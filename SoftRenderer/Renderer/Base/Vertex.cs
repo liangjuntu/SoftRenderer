911caa64415e065c21e90b6cda58aedf5d794e8b
liangjuntu <512784325@qq.com>
2019-01-25 21:24:56 +0800

    Revert "update clip space backface culling"
    This reverts commit c7251fc065bd0a01b95b543bbf56a303cdabfc81.

--- SoftRenderer/Renderer/Base/Vertex.cs
+++ SoftRenderer/Renderer/Base/Vertex.cs
@@ -40,8 +40,6 @@ namespace SoftRenderer
         public Vector2 texcoord { get; set; }
         public Vector4 color { get; set; }
 
-        public Vector3 posScreen { get; set; } //viewport坐标
-
         //Other Attributes
         public void Clone(VSOutput other)
         {
